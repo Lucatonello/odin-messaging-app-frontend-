61da776c7223b762aa2a906324c0f13111484f36
Lucatonello <lucatonellow@gmail.com>
2024-10-06 15:42:40 -0300

    display profile pics and group name on group chat topbar

--- src/routes/Groupchat.jsx
+++ src/routes/Groupchat.jsx
@@ -1,11 +1,28 @@
 import { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode';
+import arrow from '../img/back-arrow.png';
+import { useNavigate } from 'react-router-dom';  
+import styles from '../Groupchat.module.css';   
 
 function Groupchat() {
     const [messages, setMessages] = useState([])
+    const [showInfo, setShowInfo] = useState(false);
+    const [userId, setUserId] = useState(null);
+    const [groupMetadata, setGroupMetadata] = useState([])
     const { id } = useParams();
     const token = localStorage.getItem('token');
+    const navigate = useNavigate();
 
+    useEffect(() => {
+        if (token) {
+          const decoded = jwtDecode(token);
+            console.log('decoded.id', decoded.id);
+            setUserId(decoded.id);
+        }
+      }, [token]);
+
+    //get group messages
     useEffect(() => {
         fetch(`http://localhost:3000/groupChat/${id}`, {
             method: 'GET',
@@ -20,8 +37,56 @@ function Groupchat() {
             setMessages(data)
           })
     }, [id, token]);
+
+    //get group metadata
+    useEffect(() => {
+        if (userId && token) {
+          fetch(`http://localhost:3000/getUserGroupChats/${userId}`, {
+            method: 'GET',
+            headers: {
+              'Authorization': `Bearer ${token}`,
+              'Content-type': 'application/json',
+            }
+          })
+            .then(res => res.json())
+            .then(data => {
+                setGroupMetadata(data);
+              console.log('metadata: ', data)
+            })
+            .catch(error => console.error('Fetch error:', error))
+        } 
+      }, [token, userId]);
+
+    const icon = {
+        height: '35px',
+        width: '35px',
+        borderRadius: '50%',
+        cursor: 'pointer'
+    };
     return (
         <div>
+            <div className={styles.topbar}>
+                <img src={arrow} alt="<-" style={icon} onClick={() => navigate('/')}/>
+                {groupMetadata.length !== 0 && (
+                    <ul className={styles.pfpContainer}>
+                        {groupMetadata[0].profilepics.map((pic, index) => (
+                            <li key={index}>
+                                <img key={index} src={pic} alt="pfp" style={{ height: '35px', width: '35px', marginRight: '10px', borderRadius: '50%', cursor: 'pointer' }} />
+                            </li>
+                        ))}
+                        <h1 style={{ margin: '0px 0px 0px 10px' }}>{groupMetadata[0] ? groupMetadata[0].name : 'Loading...' }</h1>
+
+                    </ul>
+                )}
+                {/* <img 
+                    src={groupMetadata}
+                    alt="pfp" style={icon} 
+                    onClick={() => setShowInfo(true)} 
+                /> */}
+                <h1 className={styles.chatName}>
+
+                </h1>
+            </div>
             <ul>
                 {messages.map((message) => (
                     <li key={message.id}>
